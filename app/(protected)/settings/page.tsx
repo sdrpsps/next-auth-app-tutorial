84b35b1a3c4fad8aa4a77c864b9bdc3f09a0ee05
sunny <sunny@bytespark.app>
2024-09-25 16:16:49 +0800

    auth callback to add some field

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button"
 export default async function SettingsPage() {
   const session = await auth()
 
+
   return (
     <div>{JSON.stringify(session)}
       <form action={async () => {
--- auth.ts
+++ auth.ts
@@ -1,11 +1,46 @@
 import { PrismaAdapter } from "@auth/prisma-adapter"
-import NextAuth from "next-auth"
+import NextAuth, { type DefaultSession } from "next-auth"
 import authConfig from "./auth.config"
 import { db } from "./lib/db"
- 
- 
+import { UserRole } from "@prisma/client"
+import { getUserById } from "./data/user"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+      role: UserRole
+    } & DefaultSession["user"]
+  }
+}
+
 export const { auth, handlers, signIn, signOut } = NextAuth({
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user && token.sub) {
+        session.user.id = token.sub;
+      }
+
+      if (session.user && token.role) {
+        session.user.role = token.role as UserRole;
+      }
+
+      return session;
+    },
+    async jwt({ token }) {
+      if (!token.sub) return token;
+
+      const existingUser = await getUserById(token.sub);
+
+      if (!existingUser) return token
+
+      token.role = existingUser.role;
+
+      return token;
+    }
+  },
   adapter: PrismaAdapter(db),
   session: { strategy: "jwt" },
   ...authConfig,
 })
+
