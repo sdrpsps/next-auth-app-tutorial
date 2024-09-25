1162a97398226ba53a08c232eba71b94784fce66
sunny <sunny@bytespark.app>
2024-09-25 13:52:05 +0800

    prisma setup and create user

--- actions/register.tsx
+++ actions/register.tsx
@@ -2,6 +2,9 @@
 
 import { RegisterSchema } from "@/schemas"
 import { z } from "zod"
+import bcrypt from "bcrypt"
+import { db } from "@/lib/db"
+import { getUserByEmail } from "@/data/user"
 
 export async function register(values: z.infer<typeof RegisterSchema>) {
   const validatedFields = RegisterSchema.safeParse(values)
@@ -11,6 +14,28 @@ export async function register(values: z.infer<typeof RegisterSchema>) {
       error: Object.values(validatedFields.error.flatten().fieldErrors).join(", "),
     }
   }
+
+  const { email, password, name } = validatedFields.data
+
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return {
+      error: "Email already exists",
+    }
+  }
+
+  await db.user.create({
+    data: {
+      email,
+      name,
+      password: hashedPassword,
+    },
+  })
+
+  // TODO: Send verification email
 
   return {
     success: "Register successful",
